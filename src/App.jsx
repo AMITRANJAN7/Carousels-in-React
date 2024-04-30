08e4b16f231b92a182eee0c408efa9d51f63feab
AMIT  RANJAN <157046786+AMITRANJAN7@users.noreply.github.com>
2024-04-30 11:35:53 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+function App() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1
+  };
+  return (
+    <div className='w-3/4 m-auto'>
+      <div className="mt-20">
+      <Slider {...settings}>
+        {data.map((d) => (
+          <div key={d.name} className="bg-blue-300 h-[450px] text-black rounded-xl">
+            <div className='h-56 bg-green-500 flex justify-center items-center rounded-t-xl'>
+              <img src={d.img} alt="" className="h-44 w-44 rounded-full"/>
+            </div>
+
+            <div className="flex flex-col items-center justify-center gap-4 p-4">
+              <p className="text-xl font-semibold">{d.name}</p>
+              <p className="text-center">{d.review}</p>
+              <button className='bg-cyan-500 text-black text-lg px-6 py-1 rounded-xl'>Read More</button>
+            </div>
+          </div>
+        ))}
+      </Slider>
+      </div>
+      
+    </div>
+  );
+}
+
+const data = [
+  {
+    name: `Yogendra Singh`,
+    img: `/students/yogendra-singh.jpg`,
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+  },
+  {
+    name: `Abdallaa Alebrahim`,
+    img: `/students/abdallaa-alebrahim.jpg`,
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+  },
+  {
+    name: `Victor Forgas`,
+    img: `/students/viktor-forgacs.jpg`,
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+  },
+  {
+    name: `Emila-Der`,
+    img: `/students/albert-der.jpg`,
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+  },
+  {
+    name: `Kazi-Mizan`,
+    img: `/students/kazi-mizan.jpg`,
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+  },
+  {
+    name: `Dorothy Jones`,
+    img: `/students/dorothy-jones.jpg`,
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+  },
+  
+];
+
+export default App;
